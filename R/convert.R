9b379faf85928bf4d2444eea24cbae1e05a1214d
stla <laurent_step@yahoo.fr>
2016-02-05 12:52:42 +0100

    pspdf2img

--- R/convert.R
+++ R/convert.R
@@ -2,17 +2,18 @@
 #' 
 #' 
 #' @export
-ps2img <- function(psfile, format="gif", ps2eps=FALSE){
-  pngfile <- paste0(knitr:::sans_ext(psfile), ".", format)
+pspdf2img <- function(infile, format="png", moreopts="", ps2eps=FALSE){
+  outfile <- paste0(knitr:::sans_ext(infile), ".", format)
   if(ps2eps){
-    command <- sprintf("ps2eps -f %s", psfile)
+    command <- sprintf("ps2eps -f %s", infile)
     system(command)
-    psfile <- paste0(knitr:::sans_ext(psfile), ".eps")
+    infile <- paste0(knitr:::sans_ext(infile), ".eps")
   }
-  command <- ifelse(format=="gif", sprintf("convert -trim %s %s", psfile, pngfile),
-                    sprintf("gs -r300 -dEPSCrop -dTextAlphaBits=4 -sDEVICE=png16m -sOutputFile=%s -dBATCH -dNOPAUSE %s", pngfile, psfile)
-                    #sprintf("gs -r300 -dTextAlphaBits=4 -sDEVICE=png16m -sOutputFile=%s -dBATCH -dNOPAUSE %s", pngfile, psfile)
-                    )
+  command <- sprintf("convert -trim -transparent white -density 300 %s %s %s", moreopts, infile, outfile)
+#   command <- ifelse(format=="gif", sprintf("convert -trim %s %s", psfile, pngfile),
+#                     sprintf("gs -r300 -dEPSCrop -dTextAlphaBits=4 -sDEVICE=png16m -sOutputFile=%s -dBATCH -dNOPAUSE %s", pngfile, psfile)
+#                     #sprintf("gs -r300 -dTextAlphaBits=4 -sDEVICE=png16m -sOutputFile=%s -dBATCH -dNOPAUSE %s", pngfile, psfile)
+#                     )
   # ça coupe le bas, finalement: convert -density 300 in.eps out.png
   # aussi -resize 600x400 -transparent white
 
